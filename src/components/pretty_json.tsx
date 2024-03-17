98de68976db6fd40161f38cca8627f2f67ef0e81
Michael Spencer <mwspencer75@gmail.com>
2024-03-16 18:49:46 -0700

    added clear button

--- src/components/pretty_json.tsx
+++ src/components/pretty_json.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import arrow from "~/../public/arrow-big-right.png";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { IoIosCopy } from "react-icons/io";
 
 const Pretty_JSON = () => {
@@ -20,6 +21,11 @@ const Pretty_JSON = () => {
     console.log(prettyJson);
   };
 
+  const onClear = () => {
+    setUserInput("");
+    setPrettyJson("");
+  };
+
   return (
     <>
       <div className="flex h-screen w-full flex-col">
@@ -29,6 +35,7 @@ const Pretty_JSON = () => {
             className="m-3 h-5/6 w-5/6 rounded-lg border border-gray-50 p-2.5 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             placeholder="Your json here..."
             onChange={(e) => setUserInput(e.target.value)}
+            value={userInput}
           ></textarea>
 
           <div className="flex h-full items-center justify-center">
@@ -55,6 +62,12 @@ const Pretty_JSON = () => {
             onClick={onPretty}
           >
             Pretty-fy My JSON!
+          </button>
+          <button
+            className="my-1 ml-2 rounded bg-[#DB1F48] p-4 px-4 py-2 font-bold text-white hover:bg-red-700"
+            onClick={onClear}
+          >
+            Clear!
           </button>
         </div>
       </div>
